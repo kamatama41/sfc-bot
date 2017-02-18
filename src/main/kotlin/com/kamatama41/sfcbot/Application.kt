7543f668b1cfddeaca180cdfbbd8becdfb8bfd8d
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-02-18 13:40:08 +0900

    Make simple

--- src/main/kotlin/com/kamatama41/sfcbot/Application.kt
+++ src/main/kotlin/com/kamatama41/sfcbot/Application.kt
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class Application {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
-        }
-    }
+open class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
 }
